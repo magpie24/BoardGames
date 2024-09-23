30fab19ed003d3c0aa894787aa9af867bb807127
magpie24 <eaa23mapi@students.eaaa.dk>
2024-09-23 10:40:01 +0200

    Name Change

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  base: '/konczeztym/', // Replace with your repository name
+  base: '/BoardGames/', 
   plugins: [react()],
 });
